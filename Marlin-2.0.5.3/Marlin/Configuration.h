f486d626a082b6f4931e00b84b37d249f69abbb5
guigas <guigashm@hotmail.com>
2020-04-20 20:09:44 +0100

    V15 - Z stepps 398.44 mesured for 100mm linear advance K0.4

--- Marlin-2.0.5.3/Marlin/Configuration.h
+++ Marlin-2.0.5.3/Marlin/Configuration.h
@@ -735,7 +735,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 390, 445 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 398.44, 445 }
 
 /**
  * Default Max Feed Rate (mm/s)
