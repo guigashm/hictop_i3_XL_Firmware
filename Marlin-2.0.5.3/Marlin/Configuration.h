6b68574da67191e2fbc7abb11d82b73351a76c67
guigas <guigashm@hotmail.com>
2020-04-20 23:37:08 +0100

    V16 - accel P600 R600 (limit for tian extruder.. avoid click noises) feed E 7 -> 8

--- Marlin-2.0.5.3/Marlin/Configuration.h
+++ Marlin-2.0.5.3/Marlin/Configuration.h
@@ -742,7 +742,7 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 300, 300, 8, 7 }
+#define DEFAULT_MAX_FEEDRATE          { 300, 300, 8, 8 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
@@ -770,8 +770,8 @@
  *   M204 R    Retract Acceleration
  *   M204 T    Travel Acceleration
  */
-#define DEFAULT_ACCELERATION          900    // X, Y, Z and E acceleration for printing moves
-#define DEFAULT_RETRACT_ACCELERATION  350    // E acceleration for retracts
+#define DEFAULT_ACCELERATION          600    // X, Y, Z and E acceleration for printing moves
+#define DEFAULT_RETRACT_ACCELERATION  600    // E acceleration for retracts
 #define DEFAULT_TRAVEL_ACCELERATION   2000    // X, Y, Z acceleration for travel (non printing) moves
 
 /**
